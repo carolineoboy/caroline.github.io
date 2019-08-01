274dc3b7c752524de308dab0235a25b48651cba1
Caroline O'Boy <carolineoboy@MacBook-Air-6.local>
2019-08-01 13:09:33 -0400

    Andrew Benintendi Fanpage

---
+++ benny_slides.js
@@ -0,0 +1,27 @@
+var slideIndex = 1; //what slide you're on
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+//should display a slide
+  var i;
+  var slides = document.getElementsByClassName("mySlide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
